43f6853ec5948c11438e49617b72b88f41bdddab
Mounir Aiache <aiamounirpro@gmail.com>
2023-12-26 14:46:03 -0500

    set up frontend

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // path of the file for the content to analyse to generate the classes that I use
+  content: ["./dist/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
