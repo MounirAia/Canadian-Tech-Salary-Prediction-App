034e0265f52ee38b9f288dd038d497d0d1c826c6
Mounir Aiache <aiamounirpro@gmail.com>
2023-12-28 15:03:58 -0500

    Changed design of the index page to be more modern

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,7 +3,21 @@ module.exports = {
   // path of the file for the content to analyse to generate the classes that I use
   content: ["./dist/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          300: "#F7F9FB",
+          400: "#E3F5FF",
+        },
+        secondary: {
+          400: "#E5ECF6",
+        },
+        accent: {
+          300: "#333333",
+          400: "#1C1C1C",
+        },
+      },
+    },
   },
   plugins: [],
 };
