fdfc735ec8a13bf6aa1591a5821f8f7f4402c494
Mounir Aiache <aiamounirpro@gmail.com>
2023-12-28 15:03:58 -0500

    Changed design of the index page to be more modern

--- frontend/dist/js/my-chart.js
+++ frontend/dist/js/my-chart.js
@@ -3,10 +3,9 @@ var data = {
   labels: ["Label 1", "Label 2", "Label 3", "Label 4"],
   datasets: [
     {
-      label: "Sample Data",
-      backgroundColor: "rgba(239 ,68 ,68)", // Color for bars
-      borderColor: "black", // Border color for bars
-      borderWidth: 2, // Border width for bars
+      backgroundColor: "#b6c4cc", // Color for bars
+      borderRadius: 10,
+      // borderWidth: 2, // Border width for bars
       data: [12, 19, 3, 5], // Actual data values
     },
   ],
@@ -15,19 +14,52 @@ var data = {
 // Chart configuration
 var options = {
   scales: {
+    x: {
+      grid: {
+        display: false,
+      },
+    },
     y: {
+      grid: {
+        display: false,
+      },
+    },
+  },
+  plugins: {
+    title: {
       display: true,
-      beginAtZero: true,
+      text: "Average Salaries in 10 Canadian Cities",
+      color: "#1c1c1c",
+      font: {
+        size: 20,
+        weight: "bold",
+        family: "'Inter', 'sans-serif'",
+        lineHeight: "1.75rem",
+      },
+      align: "start",
+      padding: {
+        top: 0,
+        bottom: 20,
+      },
     },
   },
   maintainAspectRatio: false,
-  devicePixelRatio: 4,
 };
 
-const ctx = document.getElementById("test2").getContext("2d");
+let ctx = document.getElementById("test2").getContext("2d");
 
 // Create the bar chart
 const myBarChart = new Chart(ctx, {
+  type: "bar", // Specify the chart type
+  data: data,
+  options: options,
+  responsive: true,
+});
+
+ctx = document.getElementById("test3").getContext("2d");
+
+// Create the bar chart
+new Chart(ctx, {
   type: "bar", // Specify the chart type
   data: data,
   options: options,
