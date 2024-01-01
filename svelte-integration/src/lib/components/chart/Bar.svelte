42e0948d565111bfef871fc5fa12dc6b8efcd9ac
Mounir Aiache <aiamounirpro@gmail.com>
2024-01-01 08:23:44 -0500

    refactor frontend to use svelte

---
+++ svelte-integration/src/lib/components/chart/Bar.svelte
@@ -0,0 +1,81 @@
+<script lang="ts">
+	import Chart, { type ChartOptions } from 'chart.js/auto';
+	import { onMount } from 'svelte';
+
+	export let title = 'Bar Chart';
+	export let labels = [
+		'Label 1',
+		'Label 2',
+		'Label 3',
+		'Label 4',
+		'Label 5',
+		'Label 6',
+		'Label 7',
+		'Label 8',
+		'Label 9',
+		'Label 10'
+	];
+	export let data = [12, 19, 3, 5, 44, 12, 3, 1, 3, 66];
+	let canvas: HTMLCanvasElement;
+
+	onMount(() => {
+		// Sample data
+		let dataConfig = {
+			labels,
+			datasets: [
+				{
+					backgroundColor: '#b6c4cc', // Color for bars
+					borderRadius: 10,
+					// borderWidth: 2, // Border width for bars
+					data // Actual data values
+				}
+			]
+		};
+
+		// Chart configuration
+		const options: ChartOptions = {
+			responsive: true,
+			scales: {
+				x: {
+					grid: {
+						display: false
+					}
+				},
+				y: {
+					grid: {
+						display: false
+					}
+				}
+			},
+			plugins: {
+				title: {
+					display: true,
+					text: title,
+					color: '#1c1c1c',
+					font: {
+						size: 20,
+						weight: 'bold',
+						family: "'Arial', 'sans-serif'",
+						lineHeight: '1.75rem'
+					},
+					align: 'start',
+					padding: {
+						top: 0,
+						bottom: 20
+					}
+				}
+			},
+			maintainAspectRatio: false
+		};
+
+		// Create the bar chart
+		new Chart(canvas, {
+			type: 'bar', // Specify the chart type
+			data: dataConfig,
+			options
+		});
+		console.log('Bar has mounted');
+	});
+</script>
+
+<canvas class="bg-primary-300" bind:this={canvas}></canvas>
--- svelte-integration/src/routes/+page.svelte
+++ svelte-integration/src/routes/+page.svelte
@@ -1,4 +1,116 @@
-<h1 class="text-3xl font-bold hover:underline">
-    Hello world!
-  </h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import Heading from '$components/base/Heading.svelte';
+	import Bar from '$components/chart/Bar.svelte';
+	import Table from '$components/chart/Table.svelte';
+	import { onMount } from 'svelte';
+
+	let selectStatements: Map<string, { options: { output: string; value: string | number }[] }> =
+		new Map([
+			['Company Size', { options: [] }],
+			['Industry', { options: [] }],
+			['Title', { options: [] }],
+			['Years of Experience', { options: [] }],
+			['Country', { options: [] }],
+			['City', { options: [] }]
+		]);
+
+	onMount(() => {
+		console.log('Page has mounted');
+	});
+</script>
+
+<div class="grid grid-cols-1 grid-rows-1 gap-y-10 w-full mt-16 ml-64">
+	<main class="col-span-1 ml-8 mr-72 mb-24">
+		<section class="fields w-full mb-24">
+			<Heading headingType="h2" customClass="mb-5">Evaluate</Heading>
+			<div class="grid grid-cols-10 gap-x-8 gap-y-10">
+				{#each [...selectStatements] as [title, { options }]}
+					<select
+						class="col-span-1 py-3 px-4 border border-gray-300 text-sm rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+					>
+						<option disabled selected value="">{title}</option>
+						{#each options as option}
+							<option value={option.value}>{option.output}</option>
+						{/each}
+					</select>
+				{/each}
+
+				<button
+					class="col-start-8 col-span-3 px-3 py-3 bg-blue-200 hover:bg-blue-100 rounded max-h-10 flex items-center justify-center"
+				>
+					Evaluate
+				</button>
+
+				<div class="col-span-2 min-h-28 p-6 flex flex-col gap-5 bg-[#E3F5FF] rounded-2xl">
+					<p class="text-base font-medium">Yearly base salary in CAD</p>
+					<Heading headingType="h3" customClass="tracking-wider">112 000 $</Heading>
+				</div>
+				<div class="col-span-2 min-h-28 p-6 flex flex-col gap-5 bg-[#E5ECF6] rounded-2xl">
+					<p class="text-base font-medium">Hourly rate for 40-hour week</p>
+					<Heading headingType="h3" customClass="tracking-wider">32$</Heading>
+				</div>
+				<div class="col-span-3 min-h-28 p-6 flex flex-row text-white bg-accent-300 rounded-2xl">
+					<div class="flex flex-col gap-5 min-w-[80%]">
+						<Heading headingType="h3" customClass="tracking-wider">Your Support Counts</Heading>
+						<p class="text-base font-medium">
+							You can enhance the machine learning model quality by helping us!
+						</p>
+					</div>
+					<a
+						href="/"
+						class="bg-white hover:bg-slate-200 transition text-black text-base self-end rounded px-3 py-1 w-full flex flex-row justify-between items-center"
+					>
+						<p>Help Us</p>
+						<span class="material-symbols-outlined"> east </span>
+					</a>
+				</div>
+			</div>
+		</section>
+		<section class="dashboard w-full">
+			<Heading headingType="h2" customClass="mb-5">Dashboard</Heading>
+			<div class="grid grid-cols-10 gap-8">
+				<div class="col-span-4 flex flex-col gap-4 bg-secondary-300 rounded-xl p-6">
+					<Heading headingType="h4">Expected Salary Based on Experience</Heading>
+					<Table
+						tableWidth="w-full"
+						tableColumns={['Experience', 'Yearly Salary (CAD)', 'Hourly Salary (CAD)']}
+						tableRows={[
+							['Yes', '22', 'Acceptable'],
+							['Yes', '22', 'Acceptable'],
+							['Yes', '22', 'Acceptable'],
+							['Yes', '22', 'Acceptable']
+						]}
+					/>
+				</div>
+
+				<div class="col-span-6 p-6 bg-secondary-300 rounded-xl">
+					<Bar title="Average Salaries in 10 Canadian Cities" />
+				</div>
+				<div class="col-span-6 p-6 bg-secondary-300 rounded-xl">
+					<Bar
+						title="Average Salary per Role in the City"
+						labels={['a', 'b', 'c']}
+						data={[1, 2, 3]}
+					/>
+				</div>
+				<div class="col-span-4 flex flex-col gap-4 bg-secondary-300 rounded-xl p-6">
+					<Heading headingType="h4">Expected Salary Based on Experience</Heading>
+
+					<Table
+						tableWidth="col-span-4"
+						tableColumns={['Experience', 'Yearly Salary (CAD)', 'Hourly Salary (CAD)']}
+						tableRows={[
+							['Yes', '22', 'Acceptable'],
+							['Yes', '22', 'Acceptable'],
+							['Yes', '22', 'Acceptable'],
+							['Yes', '22', 'Acceptable']
+						]}
+					/>
+				</div>
+			</div>
+		</section>
+	</main>
+</div>
+
+<!-- <script src="../node_modules/chart.js/dist/chart.umd.js"></script>
+<script src="./js/my-chart.js"></script>  -->
