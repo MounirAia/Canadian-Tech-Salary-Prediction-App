9820c1c0d7faef5cba8b48977b58151e02032156
Mounir Aiache <aiamounirpro@gmail.com>
2024-01-08 15:37:23 -0500

    fix color parameter overview card

--- frontend/src/lib/components/base/OverviewCard.svelte
+++ frontend/src/lib/components/base/OverviewCard.svelte
@@ -7,7 +7,7 @@
 	export let color: 'bg-[#E5ECF6]' | 'bg-[#E3F5FF]' = 'bg-[#E5ECF6]';
 </script>
 
-<div class="{width} {color} min-h-28 px-6 py-8 flex flex-col gap-5 bg-[#E5ECF6] rounded-2xl">
+<div class="{width} {color} min-h-28 px-6 py-8 flex flex-col gap-5 {color} rounded-2xl">
 	<p class="text-base font-medium">
 		{title}
 	</p>
